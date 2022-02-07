f5b2db4628d1f45cf7b6597ca9bd68580799541b
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2022-02-07 20:56:01 +0100

    update logo

--- app/env.php
+++ app/env.php
@@ -11,7 +11,7 @@ $env_urlFile = "./extension/";
 
 $env_logo = "assets/LogoBatirPositif.png";
 $env_logoUrl = "assets/IconeBatirPositif.png";
-$env_logoTransp = "assets/LogoBatirPositifT.png";
+$env_logoTransp = "assets/Logo.png";
 
 
 $env_navImg = "https://37.media.tumblr.com/8b4969985e84b2aa1ac8d3449475f1af/tumblr_n3iftvUesn1snvqtdo1_1280.jpg";
